5f5949b85b4b4268771d350c1ea3269e154f6385
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2022-12-19 00:25:03 -0600

    Update BigBangValues struct
    Use rfc3339 timestamp, update party-uuids field, populate parties metadata

--- pkg/bigbangoscal/bigbangoscal.go
+++ pkg/bigbangoscal/bigbangoscal.go
@@ -1,6 +1,8 @@
 package bigbangoscal
 
 import (
+	"time"
+
 	"github.com/defenseunicorns/bigbang-oscal-component-generator/internal/bigbang"
 	"github.com/defenseunicorns/bigbang-oscal-component-generator/internal/types"
 	"gopkg.in/yaml.v2"
@@ -9,8 +11,8 @@ import (
 func BuildBigBangOscalDocument() (string, error) {
 	var bigBangOscalDocument types.OscalComponentDocument
 	var components []types.OscalComponent
+	var rfc3339Time = time.Now().Format(time.RFC3339)
 
-	bigBangOscalDocument.ComponentDefinition.Metadata.Title = "Big Bang"
 	documents, version, err := bigbang.GetAllBigBangSubchartOscalComponentDocuments()
 	if err != nil {
 		return "", err
@@ -18,9 +20,15 @@ func BuildBigBangOscalDocument() (string, error) {
 	for _, doc := range documents {
 		components = append(components, doc.ComponentDefinition.Components...)
 	}
-	bigBangOscalDocument.ComponentDefinition.Components = components
 	bigBangOscalDocument.ComponentDefinition.Metadata.Title = "Big Bang"
+	bigBangOscalDocument.ComponentDefinition.Metadata.LastModified = rfc3339Time
 	bigBangOscalDocument.ComponentDefinition.Metadata.Version = version
+	bigBangOscalDocument.ComponentDefinition.Metadata.Parties.Type = "organization"
+	bigBangOscalDocument.ComponentDefinition.Metadata.Parties.Name = "Platform One"
+	bigBangOscalDocument.ComponentDefinition.Metadata.Parties.Links.Href = "<https://p1.dso.mil>"
+	bigBangOscalDocument.ComponentDefinition.Metadata.Parties.Links.Rel = "website"
+	bigBangOscalDocument.ComponentDefinition.Components = components
+
 	yamlDocBytes, err := yaml.Marshal(bigBangOscalDocument)
 	if err != nil {
 		return "", err
